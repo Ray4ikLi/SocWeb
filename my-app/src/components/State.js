e599d6b7318c421a5bf22f5cc68e77c493e34a32
Ray4ikLi <123280982+Ray4ikLi@users.noreply.github.com>
2023-01-26 22:34:35 +0300

    Рефакторинг, внедрениe STORE

--- my-app/src/components/State.js
+++ my-app/src/components/State.js
@@ -1,56 +1,58 @@
-let rerenderEntireTree = () => {
-
- }
-
-
-let state = {
-    MessagesPage: {
-        MessagesData: [
-            {id: 1, message: 'Ку-ку'},
-            {id: 2, message: 'Я гуль'},
-            {id: 3, message: 'Шиза'},
-        ],
-        DialogsData: [
-            {id: 1, name: 'Ярик'},
-            {id: 2, name: 'Даша'},
-            {id: 3, name: 'Паша'},
-            {id: 4, name: 'Максим'},
-        ],
+let store = {
+    _state: {
+        MessagesPage: {
+            MessagesData: [
+                {id: 1, message: 'Ку-ку'},
+                {id: 2, message: 'Я гуль'},
+                {id: 3, message: 'Шиза'},
+            ],
+            DialogsData: [
+                {id: 1, name: 'Ярик'},
+                {id: 2, name: 'Даша'},
+                {id: 3, name: 'Паша'},
+                {id: 4, name: 'Максим'},
+            ],
+        },
+        ProfilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 11},
+                {id: 2, message: 'Its my first post', likesCount: 4},
+            ],
+            newPostText: 'Enter text',
+        },
+        Sidebar: {
+            friends: [
+                {id: 1, friendName: 'Даша'},
+            ]
+        },
     },
-    ProfilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 11},
-            {id: 2, message: 'Its my first post', likesCount: 4},
-        ],
-        newPostText: 'Enter text',
+    getState() {
+        return this._state
     },
-    Sidebar: {
-        friends: [
-            {id: 1, friendName:'Даша'},
-        ]
+    _callSubscriber() {
+        console.log('asdsadas')
     },
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.ProfilePage.newPostText,
+            likesCount: 0,
+        };
+        this._state.ProfilePage.posts.push(newPost);
+        this._state.ProfilePage.newPostText = '';
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newText) {
+        this._state.ProfilePage.newPostText = newText;
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; // паттерн = наблюдатель = обсервер // asdsad
+    }
+
 }
 
-window.state = state
+export default store;
 
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.ProfilePage.newPostText,
-        likesCount: 0,
-    };
-    state.ProfilePage.posts.push(newPost);
-    state.ProfilePage.newPostText = '';
-    rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newText) => {
-    state.ProfilePage.newPostText = newText;
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; // паттерн = наблюдатель = обсервер // asdsad
-}
-
-export default state;
+window.state = store;
+//store - OOP
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {addPost, subscribe, updateNewPostText} from "./components/State";
+import store from "./components/State";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </React.StrictMode>
     );
 }
-rerenderEntireTree(state)
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
